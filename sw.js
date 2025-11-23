8598701b4fa0cd7b6004a0d2de5a43a6cf34ecb4
awadhalkelali01 <awadh.alkelali.01@gmail.com>
2025-11-23 17:09:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "mywallet-v5.2";
+const CACHE_VERSION = "mywallet-v5.3";
 const CACHE_NAME = `${CACHE_VERSION}-cache`;
 
 const ASSETS = [
@@ -75,6 +75,7 @@ self.addEventListener("activate", event => {
     )
   );
 });
+
 
 
 
